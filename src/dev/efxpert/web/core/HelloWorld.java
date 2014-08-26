d11c49b854ab6e96b7f092a1ec0c2463bef811fd
AlvineLee <moogfox@gmail.com>
2014-08-26 14:11:52 +0900

    test

---
+++ src/dev/efxpert/web/core/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package dev.efxpert.web.core;
+
+/**
+ * @author moogfox
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(testMethod("Hello World211!!"));
+	}
+
+	public static String testMethod(String test){
+		
+		return  test;
+	}
+}
